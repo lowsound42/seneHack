461d51e5b9e3c2b8e48888bae7fad1b901207a34
Omar Khan <o.khandxb@gmail.com>
2021-03-02 16:31:30 -0500

    initial commit

---
+++ src/client.js
@@ -0,0 +1,20 @@
+function missYou() {
+    const data = {
+        text: 'red'
+    };
+
+    fetch('http://24.212.130.181:8042/light', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
