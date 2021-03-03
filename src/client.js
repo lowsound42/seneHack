5371dc88adcc21f66f536e3d4a32be548d8642d3
Wonkeun No <zakk23v@gmail.com>
2021-03-02 20:48:50 -0500

    weather info, a little bit of style added

--- src/client.js
+++ src/client.js
@@ -1,22 +1,116 @@
-//hello Omar
-//
 function missYou() {
     const data = {
-        text: 'red'
+        text: 'green'
     };
 
     fetch('http://24.212.130.181:8042/light', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then((response) => response.json())
-        .then((data) => {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json()) //parse JSON into object
+        .then(data => {
             console.log('Success:', data);
         })
         .catch((error) => {
             console.error(error);
         });
 }
+
+function beingSleepless() {
+    const data = {
+        text: 'red'
+    };
+    fetch('http://24.212.130.181:8042/light', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json()) //parse JSON into object
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+function excited() {
+    const data = {
+        text: 'blue'
+    };
+    fetch('http://24.212.130.181:8042/light', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json()) //parse JSON into object
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+const KELVIN = 273;
+let weather = {};
+weather.temperature = {
+    unit: "celsius"
+}
+const key = "48c7cf84c45dc694e28933f6b91317b9";
+const tempElement = document.getElementById('temperature');
+const weatherDescElement = document.getElementById('weather-desc');
+const locationElement = document.getElementById('location');
+
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    console.log(api); //min/max/feel-like temp, sunrise, sunset, location... 
+    fetch(api)
+        .then(response => response.json())
+        .then(data => {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].main;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+            // console.log(weather.temperature.value);
+            // console.log(weather.description);
+            // console.log(weather.city);
+        })
+        .then(() => {
+            displayWeahter();
+        })
+}
+
+function displayWeahter() {
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    weatherDescElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = "<p>Browser doesn't support Geolocation</p>";
+}
+
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+function showError(error) {
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = "<p> ${error.message} </p>";
+}
+//tempElement.innerHTML = `${weather.temperature.value} degree <span>C</span>`;
